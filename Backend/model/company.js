340ef748f2e08b2d966085f4b8f6b39af011f2bb
KARNA-2006 <shruti.kittu06@gmail.com>
2025-12-22 15:21:53 +0530

    added interviewer & company models

---
+++ Backend/model/company.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const companySchema = new Schema({
+  role: {
+    type: String,
+    enum: ['company'],
+    default: 'company',
+    required: true,
+  },
+  name: { type: String, required: true }, // user full name
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  companyName: { type: String, required: true },
+  location: { type: String, required: true },
+  website: { type: String },
+  hiringNeeds: { type: String },
+}, { timestamps: true });
+
+export default mongoose.model("Company", companySchema);
---
+++ Backend/model/interviewer.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const interviewerSchema = new Schema({
+  role: {
+    type: String,
+    enum: ['interviewer'], // Only interviewer for this schema
+    default: 'interviewer',
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  skills: {
+    type: String,
+    required: true,
+  },
+  experience: {
+    type: String,
+    required: true,
+  },
+  portfolio: {
+    type: String,
+  },
+  hourlyRate: {
+    type: String,
+  },
+}, { timestamps: true }); // Adds createdAt and updatedAt automatically
+
+export default mongoose.model("Interviewer", interviewerSchema);
