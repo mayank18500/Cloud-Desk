b8553531475e21ef54d9ef1a384b3c1d99a1cd9e
KARNA-2006 <shruti.kittu06@gmail.com>
2025-12-22 14:42:49 +0530

    tested apis and setup db

--- Backend/model/User.js
+++ Backend/model/User.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 
+
 const UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
@@ -8,14 +9,13 @@ const UserSchema = new mongoose.Schema({
   role: { type: String, enum: ['interviewer','company'], required: true },
   bio: String,
   skills: [String],
-  pricing: PricingSchema,
+  pricing:{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "PricingSchema" ,
+  },
   portfolio: [String],
   createdAt: { type: Date, default: Date.now }
 });
 
 export default mongoose.model('User', UserSchema);
 
-const PricingSchema = new mongoose.Schema({
-  hourlyRate: Number,
-  projectRate: Number
-});
--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import mongoose from "mongoose";
+import User from "./model/User.js";
 
 dotenv.config();
 const app=express();
@@ -8,8 +9,33 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const PORT=process.env.PORT || 5000;
 
+main()
+    .then(()=>{
+        console.log("connected to db");
+
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
+async function main(){
+    await mongoose.connect("mongodb://127.0.0.1:27017/Cloud-Desk");
+};
+
 app.get("/",(req,res)=>{
     res.status(200).send("Hire Hub Backend Server is Running");
+});
+
+app.post("/User",async (req,res)=>{
+    try{
+        const {name,email,password,role,bio,skills,pricing,portfolio}= req.body
+        const newUser= new  User({name,email,password,role,bio,skills,pricing,portfolio})
+        await newUser.save();
+        res.status(201).json({ success: true, message: "User saved", data: newUser});
+    }
+    
+    catch (err) {
+    console.error(err);
+    }
 });
 
 app.listen(PORT,()=>{
